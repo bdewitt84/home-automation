31f1a545e712752dff8276c88b2bb4fee674fe0a
Brett DeWitt <38679865+bdewitt84@users.noreply.github.com>
2025-12-04 19:15:22 -0800

    dependencies: refactor providers.py to use reusable getter
    A new helper function `_get_dependency` is now used by wrapper functions which are passed to the routers.
    
    This promotes consistency and eliminates duplicated code.

--- app/dependencies/providers.py
+++ app/dependencies/providers.py
@@ -1,57 +1,14 @@
 # app/dependencies/providers.py
 
 from app.container import DependencyContainer
-from interfaces.media_control_interface import MediaControlInterface
-from events.event_bus import ASyncEventBus
 from fastapi import Request, HTTPException
 from app.dependencies.container_keys import (
-    MEDIA_CONTROL_KEY,
-    CONTAINER_KEY,
-    EVENT_BUS_KEY,
-    MEDIA_CONTROL_SERVICE_KEY
+    CONTAINER_KEY, MEDIA_CONTROL_SERVICE_KEY,
 )
-from services.media.media_control_service import MediaControlService
 
 
-def get_media_control_service(request: Request) -> MediaControlService:
+def _get_dependency(key: str, container: DependencyContainer) -> any:
     try:
-        container: DependencyContainer = getattr(request.app.state, CONTAINER_KEY)
-        media_control_service = container.resolve(MEDIA_CONTROL_SERVICE_KEY)
-        return media_control_service
-    except KeyError:
-        raise HTTPException(
-            status_code=500,
-            detail="Application misconfiguration: media control service not registered."
-        )
-
-
-def get_media_controller(request: Request) -> MediaControlInterface:
-
-    try:
-        container: DependencyContainer = getattr(request.app.state, CONTAINER_KEY)
-        media_controller = container.resolve(MEDIA_CONTROL_KEY)
-        return media_controller
-
-    except KeyError:
-        raise HTTPException(
-            status_code=500,
-            detail="Application misconfiguration: media controller not registered.",
-        )
-
-def get_event_bus(request: Request) -> ASyncEventBus:
-    try:
-        container: DependencyContainer = getattr(request.app.state, CONTAINER_KEY)
-        bus = container.resolve(EVENT_BUS_KEY)
-        return bus
-    except KeyError:
-        raise HTTPException(
-            status_code=500,
-            detail="Application misconfiguration: event bus not registered.",
-        )
-
-def get_dependency(request: Request, key: str) -> any:
-    try:
-        container: DependencyContainer = getattr(request.app.state, CONTAINER_KEY)
         dependency = container.resolve(key)
         return dependency
     except KeyError:
@@ -59,3 +16,8 @@ def get_dependency(request: Request, key: str) -> any:
             status_code=500,
             detail=f"Application misconfiguration: {key} not registered.",
         )
+
+
+def get_media_control_service(request: Request):
+    container = getattr(request.app.state, CONTAINER_KEY)
+    return _get_dependency(MEDIA_CONTROL_SERVICE_KEY, container)

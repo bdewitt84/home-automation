18f76333af503ea6230d5c8b9a66cc124d67779b
Brett DeWitt <38679865+bdewitt84@users.noreply.github.com>
2025-12-06 17:06:38 -0800

    events: fix: incorrect reference
    datetime.UTC corrected to timezone.utc

--- events/base.py
+++ events/base.py
@@ -1,12 +1,12 @@
 # events/base.py
 
 from pydantic import BaseModel
-from datetime import datetime
+from datetime import datetime, timezone
 
 
 class BaseEvent(BaseModel):
 
-    time: datetime = datetime.now(datetime.UTC)
+    time: datetime = datetime.now(timezone.utc)
 
     def get_type(self):
         return self.__class__.__name__

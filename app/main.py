eb8d3e172000b4ea4c9d6f7c54ed681a97ed79fa
Brett DeWitt <38679865+bdewitt84@users.noreply.github.com>
2025-12-06 16:51:28 -0800

    project: add comments to main.py
    Explicit explanation of what is happening at the entry point.
    
    With the architecture falling into place, clear explanations will help crystallize the organization of the project.

--- app/main.py
+++ app/main.py
@@ -6,13 +6,19 @@ from app.state import configure_state, shutdown_state
 from app.container import DependencyContainer
 from app.dependencies.container_keys import CONTAINER_KEY
 
-
+# Create the dependency container
 dependency_container = DependencyContainer()
 
+# Start the FastAPI application
 app = FastAPI(description="home-automation-service")
 
+# Attach the dependency container to the application state
 setattr(app.state, CONTAINER_KEY, dependency_container)
 
+# Attach the configuration script to the startup event.
+# This script will initialize all required dependencies and store them
+# in the dependency container that factories will use to build services.
+# More details in app.state.py
 app.add_event_handler('startup', lambda: configure_state(app) )
 app.add_event_handler('shutdown', lambda: shutdown_state(app) )
 

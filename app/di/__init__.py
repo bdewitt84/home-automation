d2359a07a4c73ba76b85e9825a7157352f4f9960
Brett DeWitt <38679865+bdewitt84@users.noreply.github.com>
2025-12-13 17:24:32 -0800

    di: fix: removed deprecated provider from __init__.py
    Correct location of providers is api/providers.py

--- app/di/__init__.py
+++
@@ -1,9 +0,0 @@
-# app/di/providers.py
-
-from interfaces.media_control_interface import MediaControlInterface
-from fastapi import Request
-
-
-def get_media_controller(request: Request) -> MediaControlInterface:
-    media_controller = getattr(request.app.state, 'media_control_instance', None)
-    return media_controller

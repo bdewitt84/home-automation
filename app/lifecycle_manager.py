c2e5653433a85c824ec18d40ffabbd5aab3c65fe
Brett DeWitt <38679865+bdewitt84@users.noreply.github.com>
2025-12-21 16:47:22 -0800

    lifecycle: add error handling
    Try and except blocks added to handle errors during startup and shutdown, as well as raising when trying to index and already-indexed singleton
    
    This make application policy explicit and enforceable

--- app/lifecycle_manager.py
+++ app/lifecycle_manager.py
@@ -9,17 +9,37 @@ class LifeCycleManager:
         self._singletons: [LifecycleManagementInterface] = []
 
     def index_singleton(self, singleton: Any):
-        if isinstance(singleton, LifecycleManagementInterface):
-            print(f"Indexed {singleton.__class__.__name__} with lifecycle manager")
-            self._singletons.append(singleton)
+        # Don't index non-lifecycle instances
+        if not isinstance(singleton, LifecycleManagementInterface):
+            return
+
+        # Don't index an instance twice. This is a policy violation, so we raise
+        if singleton in self._singletons:
+            raise ValueError("Singleton already indexed")
+
+        self._singletons.append(singleton)
+        print(f"Indexed {singleton.__class__.__name__} with lifecycle manager")
 
     async def start_registered(self):
         print(f"Starting lifecycle for {[singleton.__class__.__name__ for singleton in self._singletons]}")
+
         for singleton in self._singletons:
             print(f"Calling start on {singleton.__class__.__name__}")
-            await singleton.start()
+            try:
+                await singleton.start()
+
+            # Crash if a component fails to start. No half-alive systems.
+            # lifespan will call shutdown, don't attempt to roll-back here.
+            except Exception as e:
+                print(f"Failed to start {singleton.__class__.__name__}.")
+                raise e
 
     async def stop_registered(self):
         for singleton in reversed(self._singletons):
-            print(f"Calling stop on {singleton.__class__.__name__}")
-            await singleton.stop()
+            try:
+                print(f"Stopping {singleton.__class__.__name__}")
+                await singleton.stop()
+
+            # Log and let the exception fall through. Stop as many components as possible.
+            except Exception as e:
+                print(f"Could not stop {singleton.__class__.__name__}: {e}.")

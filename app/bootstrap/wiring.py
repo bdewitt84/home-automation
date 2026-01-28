bf5459b25be7526aec61bdb1e84e4d9d83082d3a
Brett DeWitt <38679865+bdewitt84@users.noreply.github.com>
2026-01-27 16:17:22 -0800

    chore: remove unused code from wiring.py
    Gone are imports and commented out code

--- app/bootstrap/wiring.py
+++ app/bootstrap/wiring.py
@@ -8,10 +8,7 @@ import pkgutil
 
 from app.bootstrap.scanner import (
     scan_registry_decorators,
-    register_components_with_dependency_container,
-    auto_register_components_with_dependency_container,
     register_dynamic_components,
-    register_components_with_lifecycle_manager,
     register_components_with_lfm,
     register_on_startup_components,
     load_config_from_disk,
@@ -50,16 +47,12 @@ def bootstrap_application(app: FastAPI) -> None:
     register_event_bus(container)
 
     # --- Register Subprocess Singletons ---
-    # register_vlc_process_manager(container)
     scan_registry_decorators(path=SERVICE_PACKAGE_NAME,
                              module_importer=importlib.import_module,
                              package_walker=pkgutil.walk_packages, )
-    # auto_register_components_with_dependency_container(COMPONENT_METADATA_REGISTRY, container)
-    # register on startup
+
     register_on_startup_components(COMPONENT_METADATA_REGISTRY, container)
-    # register dynamic components
     register_dynamic_components(config_data, container, COMPONENT_METADATA_REGISTRY)
-    # register_components_with_lifecycle_manager(COMPONENT_METADATA_REGISTRY, container, manager)
     register_components_with_lfm(container, manager)
 
     # --- Register Component Singletons ---

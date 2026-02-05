25944d4f341c2b86e50b717d148d9fe619da7f5b
Brett DeWitt <38679865+bdewitt84@users.noreply.github.com>
2026-02-04 18:47:52 -0800

    config: remove unused models in settings.py
    Pydantic models VLCMediaControllerSettings and VLCProcessManagerSettings are removed
    
    Dynamic components now have their own pydantic models defined locally to their definitions. The models in config/settings.py are no longer needed.

--- config/settings.py
+++ config/settings.py
@@ -1,51 +1,6 @@
 # config/settings.py
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import Field
-
-
-class VLCMediaControllerSettings(BaseSettings):
-    model_config = SettingsConfigDict(
-        env_prefix='VLC_MEDIA_CONTROLLER_',
-        case_sensitive=False,
-        env_file='.env',
-        env_file_encoding='utf-8',
-    )
-
-    URL: str = Field(
-        'http://127.0.0.1:8080',
-        description='VLC HTTP interface URL'
-    )
-
-    PASSWORD: str = Field(
-        'your_password',
-        description='VLC HTTP interface password'
-    )
-
-
-class VLCProcessManagerSettings(BaseSettings):
-    model_config = SettingsConfigDict(
-        env_prefix='VLC_PROCESS_MANAGER_',
-        case_sensitive=False,
-        env_file='.env',
-        env_file_encoding='utf-8',
-    )
-
-    HOST: str = Field(
-        '127.0.0.1',
-        description='VLC HTTP interface process host mask'
-    )
-
-    PORT: str = Field(
-        '8080',
-        description='VLC HTTP interface process port'
-    )
-
-    PASSWORD: str = Field(
-        'your_password',
-        description='VLC HTTP interface process password'
-    )
-
 
 class AppSettings(BaseSettings):
     model_config = SettingsConfigDict(
@@ -54,9 +9,5 @@ class AppSettings(BaseSettings):
         env_file='.env',
         env_file_encoding='utf-8',
     )
-
-    media: VLCMediaControllerSettings = VLCMediaControllerSettings()
-    vlc: VLCProcessManagerSettings = VLCProcessManagerSettings()
-
 
 settings = AppSettings()

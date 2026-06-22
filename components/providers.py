8344b0e3f0dece48fa5b1fd6b2e67ca708e12629
Brett DeWitt <38679865+bdewitt84@users.noreply.github.com>
2026-06-21 18:57:15 -0700

    fix: `DuplicateKeyError` for `SystemService`
    The component decorator has been temporarily commented out from `system_service_provider` until we can implement lazy validation for the dependency graph.

--- components/providers.py
+++ components/providers.py
@@ -7,7 +7,7 @@ from components.services.debian_system_service import DebianSystemService
 from interfaces import SystemService
 
 
-@component(key='SystemService',
-           is_dependency=True)
+# @component(key='SystemService',
+#            is_dependency=True)
 def system_service_provider(os_utils: OSUtils) -> SystemService:
     return DebianSystemService(os_utils)

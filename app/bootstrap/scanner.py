b9f9c048a6d77b076ef9f05a5d70c6fa1dc67402
Brett DeWitt <38679865+bdewitt84@users.noreply.github.com>
2026-07-05 17:22:04 -0700

    bootstrap: reimplement scanner
    The scanning function has been re-implemented as a class
    
    fixup! project: remove old static routers

--- app/bootstrap/scanner.py
+++ app/bootstrap/scanner.py
@@ -2,15 +2,14 @@
 
 import importlib
 import pkgutil
-from typing import Type, Any, Callable, Iterator
+from pkgutil import ModuleInfo
+from typing import Type, Callable, Iterator, Iterable
 from types import ModuleType
-
-ModuleInfo = Any
 
 
 def scan_for_components(path: str,
                         module_importer: Type[Callable[[str], ModuleType]] = importlib.import_module,
-                        package_walker: Type[Callable[[str], Iterator[ModuleInfo]]] = pkgutil.walk_packages
+                        package_walker: Type[Callable[[Iterable[str]], Iterator[ModuleInfo]]] = pkgutil.walk_packages
                         ) -> None:
     """
     Imports all modules at package root 'path', forcing the registration
@@ -26,3 +25,35 @@ def scan_for_components(path: str,
         module_name = package.__name__ + '.' + name
         module_importer(module_name)
         print(f"Scanner: Imported '{module_name}'")
+
+
+class Scanner:
+    def __init__(self,
+                 package_walker: Callable[[Iterable[str]], Iterator[ModuleInfo]]=pkgutil.walk_packages,
+                 module_importer: Callable[[str], ModuleType]=importlib.import_module, ):
+        self._package_walker = package_walker
+        self._package_importer = module_importer
+        self._scanned_modules: list[str] = []
+
+    def scan_packages(self, path: list[str]) -> list[str]:
+        found = []
+        for item in path:
+            package = self._package_importer(item)
+            found.extend([
+                package.__name__ + '.' + name
+                for _finder, name, _is_pkg
+                in self._package_walker(package.__path__)
+            ])
+        self._scanned_modules.extend(found)
+        return found
+
+    def get_scanned_modules(self) -> list[str]:
+        return self._scanned_modules
+
+    def import_scanned_modules(self):
+        for module in self._scanned_modules:
+            self._package_importer(module)
+        self.clear()
+
+    def clear(self):
+        self._scanned_modules.clear()
--- app/main.py
+++ app/main.py
@@ -3,15 +3,11 @@
 from fastapi import FastAPI
 
 from app.bootstrap.lifespan import lifespan
-from api.v1.v1_router import v1_router
 
 # Start the FastAPI application
 # Bootstrapping is handled by app/bootstrap/lifespan.py
 app = FastAPI(description="home-automation-service",
               lifespan=lifespan)
-
-# Dummy router for testing
-app.include_router(v1_router, prefix="/api/v1")
 
 @app.get("/")
 async def root():

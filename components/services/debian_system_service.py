3bcc9a7304827548d26982d5485adefffc7daa55
Brett DeWitt <38679865+bdewitt84@users.noreply.github.com>
2026-07-22 15:33:02 -0700

    infrastructure: use `OsUtils` instead of python's `os`
    Component `DebianSystemService` now uses the `OsUtils` dependency instead of importing `os` from Python directly.

--- components/services/debian_system_service.py
+++ components/services/debian_system_service.py
@@ -1,11 +1,10 @@
 # components/debian_system_service.py
 
-import os
-
 from components.infrastructure.os_utils import OSUtils
 from config.project import PROJECT_ROOT
 from interfaces import SystemService
 from app.models.system import SystemResult
+
 
 UPDATE_SCRIPT_RELATIVE_PATH = "./scripts/home-automation-update.sh"
 COMMAND_REBOOT = "sudo reboot"
@@ -15,18 +14,17 @@ COMMAND_SHUTDOWN = "sudo shutdown"
 class DebianSystemService(SystemService):
     def __init__(self, os_utils: OSUtils):
         super().__init__()
-        self.os_utils = os_utils
+        self._os_utils = os_utils
 
     def reboot(self) -> SystemResult:
         cmd = COMMAND_REBOOT
-        system_result: SystemResult = self.os_utils.execute_shell_command(cmd)
+        system_result: SystemResult = self._os_utils.execute_shell_command(cmd)
         return system_result
 
     def shutdown(self) -> SystemResult:
         cmd = COMMAND_SHUTDOWN
-        return self.os_utils.execute_shell_command(cmd)
+        return self._os_utils.execute_shell_command(cmd)
 
     def update_application(self) -> SystemResult:
-        # TODO: incorporate path ops to OSUtils service
-        cmd = os.path.join(PROJECT_ROOT, UPDATE_SCRIPT_RELATIVE_PATH)
-        return self.os_utils.execute_shell_command(cmd)
+        cmd = self._os_utils.path_join(PROJECT_ROOT, UPDATE_SCRIPT_RELATIVE_PATH)
+        return self._os_utils.execute_shell_command(cmd)

f92caa3eb99dd9fee444553c86446606999f12c4
Brett DeWitt <38679865+bdewitt84@users.noreply.github.com>
2026-08-03 15:04:57 -0700

    bootstrap: add frozen config to `AppSettings`
    Added `BaseSettings.ConfigDict` with `frozen=True`.
    
    Ensures that the globally importable app settings are immutable.

--- config/settings.py
+++ config/settings.py
@@ -1,11 +1,17 @@
 # config/settings.py
 
-from pydantic_settings import BaseSettings
+from pydantic import BaseModel, ConfigDict
+
 import os
 
-
-class AppSettings(BaseSettings):
+class AppSettings(BaseModel):
+    model_config = ConfigDict(
+        frozen=True,
+    )
     PROJECT_ROOT: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     CONFIG_FILE_PATH: str = './config/config.json'
     SERVICE_PACKAGE_NAME: str = 'components'
     CONTROLLER_PACKAGE_NAME: str = 'api.v1.controllers'
+
+
+app_settings = AppSettings()

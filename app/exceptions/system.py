1808992aeff3e0d4c909fcd61b7d3a23233dfb12
Brett DeWitt <38679865+bdewitt84@users.noreply.github.com>
2026-08-18 14:15:44 -0700

    exceptions: add system exceptions
    Exceptions `ProcessError`, `ExecutableNotFoundError`, `ProcessStartupError`, `ProcessNotFoundError`, and `ProcessTerminationError` have each been added to `system.py`.

--- app/exceptions/system.py
+++ app/exceptions/system.py
@@ -1,6 +1,31 @@
 # ./app/exceptions/system.py
 
+
 class SystemOperationError(Exception):
     """ Raised for errors related to system operations. """
     pass
 
+
+class ProcessError(Exception):
+    """ Raised for errors related to system processes and subprocesses. """
+    pass
+
+
+class ExecutableNotFoundError(ProcessError):
+    """ Raised when the executable not found. """
+    pass
+
+
+class ProcessStartupError(ProcessError):
+    """ Raised when the process has failed to start. """
+    pass
+
+
+class ProcessNotFoundError(ProcessError):
+    """ Raised when the process is not found. """
+    pass
+
+
+class ProcessTerminationError(ProcessError):
+    """ Raised when the process has failed to terminate. """
+    pass

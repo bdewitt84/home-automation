b5c27372c236043d2b2e9d6c6ee2a997ac148986
Brett DeWitt <38679865+bdewitt84@users.noreply.github.com>
2026-08-25 15:38:27 -0700

    events: fix base event timestamp
    Event timestamps are now generated at instantiation. Previously, one timestamp was generated at runtime and was used for all instances.

--- events/base.py
+++ events/base.py
@@ -1,12 +1,15 @@
 # events/base.py
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import datetime, timezone
 
 
+def utc_now():
+    return datetime.now(timezone.utc)
+
 class BaseEvent(BaseModel):
 
-    time: datetime = datetime.now(timezone.utc)
+    time: datetime = Field(default_factory=utc_now)
 
     def get_type(self):
         return self.__class__.__name__
